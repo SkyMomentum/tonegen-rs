1ef4a0a40acc681d819981772cecab80cd748863
Ryan Drew <rsdrew@gmail.com>
2015-05-14 01:52:14 -0400

    Increment version because reasons. Also notice tabs then change to spaces, because reasons. Start of doc commenting existing code.

--- src/main.rs
+++ src/main.rs
@@ -8,87 +8,87 @@ use wavout::{F32Sample, DataChunk, FormatChunk, WaveHeader, WaveFile};
 
 
 fn create_sine_sample(frequency: f64, sample_rate: u32) -> Vec<F32Sample> {
-	
-	let sr: f64 = sample_rate as f64;
-	let samples_sine: f64 = sr / frequency;
-	let samples_num: u32 = samples_sine.floor() as u32;
-	let half_wave: f64 = samples_sine / 2.0f64;
-	
-	let mut tone_cycle: Vec<F32Sample> = Vec::with_capacity(samples_num as usize);
-	
-	for i in 0..samples_num {
-		let x: f64 = i as f64;
-		let val_calc: f64 = ((x * consts::PI)/half_wave).sin();
-		let val_out: f32 = val_calc as f32;
-		tone_cycle.push(val_out);
-	}
-	
-	tone_cycle
+
+    let sr: f64 = sample_rate as f64;
+    let samples_sine: f64 = sr / frequency;
+    let samples_num: u32 = samples_sine.floor() as u32;
+    let half_wave: f64 = samples_sine / 2.0f64;
+    
+    let mut tone_cycle: Vec<F32Sample> = Vec::with_capacity(samples_num as usize);
+    
+    for i in 0..samples_num {
+        let x: f64 = i as f64;
+        let val_calc: f64 = ((x * consts::PI)/half_wave).sin();
+        let val_out: f32 = val_calc as f32;
+        tone_cycle.push(val_out);
+    }
+
+    tone_cycle
 }
 
 fn generate_tone(run_length: f64, frequency: f64, sample_rate: u32, sample_bits: u32) -> DataChunk<F32Sample> {
-	
-	let mut tone_out: DataChunk<F32Sample> = Default::default();
-	
-	let tone_cycle: Vec<F32Sample> = create_sine_sample(frequency, sample_rate);
-	let cycle_len: usize = tone_cycle.len();
-	
-	let sr: f64 = sample_rate as f64;
-	let total_samples_f64: f64 = (run_length * sr).floor();
-	let total_samples: u32 =  total_samples_f64 as u32;
-	
-	let mut out_counter: u32 = 0;
-	let mut cycle_index: usize = 0;
-	
-	loop {
-		if out_counter > total_samples { break; }
-		if cycle_index >= cycle_len { cycle_index = 0; }
-		tone_out.push_sample( *(tone_cycle.get(cycle_index).unwrap()) );
-		out_counter = out_counter + 1;
-		cycle_index = cycle_index + 1;
-	}
 
-	tone_out.set_size( out_counter * (sample_bits / 8) );
-	tone_out
+    let mut tone_out: DataChunk<F32Sample> = Default::default();
+
+    let tone_cycle: Vec<F32Sample> = create_sine_sample(frequency, sample_rate);
+    let cycle_len: usize = tone_cycle.len();
+
+    let sr: f64 = sample_rate as f64;
+    let total_samples_f64: f64 = (run_length * sr).floor();
+    let total_samples: u32 =  total_samples_f64 as u32;
+
+    let mut out_counter: u32 = 0;
+    let mut cycle_index: usize = 0;
+
+    loop {
+        if out_counter > total_samples { break; }
+        if cycle_index >= cycle_len { cycle_index = 0; }
+        tone_out.push_sample( *(tone_cycle.get(cycle_index).unwrap()) );
+        out_counter = out_counter + 1;
+        cycle_index = cycle_index + 1;
+    }
+
+    tone_out.set_size( out_counter * (sample_bits / 8) );
+    tone_out
 }
 
 fn create_mono_wave_file(data_in: DataChunk<F32Sample>, sample_rate: u32, sample_bits: u32) -> WaveFile<F32Sample> {
-	
-	let format_chunk_size = 24u32;
-	let data_chunk_header_size = 8u32;
-	
-	let mut fmt: FormatChunk = Default::default();
-	let num_channels = 1u32;
-	fmt.set_sample_rate( sample_rate );
-	fmt.set_byte_rate( num_channels * sample_rate * (sample_bits/8) );
-	fmt.set_block_align( (num_channels * (sample_bits/8)) as u16 );
-	fmt.set_bits_sample( sample_bits as u16 );
-	
-	let data_size: u32 = (data_in.len() as u32) * (sample_bits / 8);
-	let total_size: u32 = data_size + format_chunk_size + data_chunk_header_size;
-	
-	let mut hdr: WaveHeader = Default::default();
-	hdr.set_size(total_size);
-	
-	let wave: WaveFile<F32Sample> = WaveFile::create_new(hdr,fmt,data_in);
-	wave
+
+    let format_chunk_size = 24u32;
+    let data_chunk_header_size = 8u32;
+
+    let mut fmt: FormatChunk = Default::default();
+    let num_channels = 1u32;
+    fmt.set_sample_rate( sample_rate );
+    fmt.set_byte_rate( num_channels * sample_rate * (sample_bits/8) );
+    fmt.set_block_align( (num_channels * (sample_bits/8)) as u16 );
+    fmt.set_bits_sample( sample_bits as u16 );
+
+    let data_size: u32 = (data_in.len() as u32) * (sample_bits / 8);
+    let total_size: u32 = data_size + format_chunk_size + data_chunk_header_size;
+
+    let mut hdr: WaveHeader = Default::default();
+    hdr.set_size(total_size);
+
+    let wave: WaveFile<F32Sample> = WaveFile::create_new(hdr,fmt,data_in);
+    wave
 }
 
 fn main() {
-	use std::io::copy;
-	
+    use std::io::copy;
+
     let y = generate_tone(2.0f64, 440.0f64, 44100, 32);
     let mut x = create_mono_wave_file(y, 44100, 32);
     //let mut z: Vec<u8> = Vec::new();
-    
+
     /*let _ = x.header.read_to_end(&mut z);
     println!("header - {:?} ", z);
     z.clear();
     let _ = x.format_chunk.read_to_end(&mut z);
     println!("fmt - {:?} ", z);*/
-    
+
     let mut f = File::create("test_440.wav").unwrap();
-    
+
     //try!( f.write_all( x.header.read_to_end()));
     let _ = copy( &mut x.header, &mut f);
     //try!( f.write_all( x.format_chunk.read_to_end()));
@@ -96,5 +96,4 @@ fn main() {
     //try!( f.write_all( x.data.read_to_end()));
     let _ = copy( &mut x.data, &mut f);
     f.sync_all();
-    
 }
--- src/wavefile/lib.rs
+++ src/wavefile/lib.rs
@@ -4,262 +4,275 @@ use std::io::{Error, ErrorKind};
 
 #[derive(Debug)]
 pub struct WaveHeader {
-	riff_header: [u8; 4], // "RIFF"
-	file_size: u32,
-	wave_header: [u8; 4], // "WAVE"
-	read_cur: usize,
+    riff_header: [u8; 4], // "RIFF"
+    file_size: u32,
+    wave_header: [u8; 4], // "WAVE"
+    read_cur: usize,
 }
 
 #[derive(Debug)]
 pub struct FormatChunk {
-	fmt_header: [u8; 4],  // "fmt "
-	size_wave_chunk: u32, // 16 - number of bytes blow
-	wave_type_format: u16,// 1 - Linear PCM
-	number_channels: u16,
-	samples_second: u32,
-	bytes_second: u32,
-	block_alignment: u16,
-	bits_sample: u16,
-	read_cur: usize,
+    fmt_header: [u8; 4],  // "fmt "
+    size_wave_chunk: u32, // 16 - number of bytes blow
+    wave_type_format: u16,// 1 - Linear PCM
+    number_channels: u16,
+    samples_second: u32,
+    bytes_second: u32,
+    block_alignment: u16,
+    bits_sample: u16,
+    read_cur: usize,
 }
 
 #[derive(Debug)]
 pub struct DataChunk<T> {
-	data_header: [u8; 4], // "data"
-	size_data: u32,
-	sample_vector: Vec<T>,
-	read_cur: usize,
+    data_header: [u8; 4], // "data"
+    size_data: u32,
+    sample_vector: Vec<T>,
+    read_cur: usize,
 }
 
 #[derive(Debug)]
 pub struct WaveFile<T> {
-	pub header: WaveHeader,
-	pub format_chunk: FormatChunk,
-	pub data: DataChunk<T>,
+    pub header: WaveHeader,
+    pub format_chunk: FormatChunk,
+    pub data: DataChunk<T>,
 }
 
 pub type F32Sample = f32;
 
-//-------------------------------------------------
-// Transmutation functions
-
+/// Transmute and copy to u8 array.
+///
+/// # Unsafe
+/// Forces $targ to be represented as an arry of u8. $num_bytes is assumed to be the number of
+/// bytes $targ occupies. $num_bytes must also be the same size as the $outbuf used. First param
+/// is just for readability at use site.
+/// 
 macro_rules! transmute_to_u8_from {
-	($t:ty, $num_bytes:expr, $targ:expr, $outbuf: expr) => ({
-			use std::ptr::copy_nonoverlapping;
-			unsafe {
-				let buf = std::mem::transmute::<_, [u8; $num_bytes]>($targ);
-				copy_nonoverlapping( (&buf).as_ptr(), $outbuf.as_mut_ptr(), $num_bytes );
-			}
-		});
+    ($t:ty, $num_bytes:expr, $targ:expr, $outbuf: expr) => ({
+            use std::ptr::copy_nonoverlapping;
+            unsafe {
+                let buf = std::mem::transmute::<_, [u8; $num_bytes]>($targ);
+                copy_nonoverlapping( (&buf).as_ptr(), $outbuf.as_mut_ptr(), $num_bytes );
+            }
+        });
 }
 
+/// Setup buffer for transmutation then call into macro.
+///
+/// The macro setups the boiler plate to call transmute_to_u8_from! macro. $fnc is a fn that exists
+/// to call transmute macro with correct parameters for converting a specific type.
+///
+/// ```
+/// fn u16_to_u8(target: u16, output: &mut [u8]) {
+///    transmute_to_u8_from!(u16, 2, target, output);
+/// }
 macro_rules! do_transmute {
-	($fnc:ident, $input:expr, $to_buf:expr, $offset:expr, $size:expr) => ({
-			let mut transmutebuf: [u8; $size] = [0; $size];
-			$fnc($input, &mut transmutebuf);
-			*$offset = *$offset + append_bytes( &transmutebuf, $to_buf, *$offset);
-		});
+    ($fnc:ident, $input:expr, $to_buf:expr, $offset:expr, $size:expr) => ({
+            let mut transmutebuf: [u8; $size] = [0; $size];
+            $fnc($input, &mut transmutebuf);
+            *$offset = *$offset + append_bytes( &transmutebuf, $to_buf, *$offset);
+        });
 }
 
 fn u16_to_u8(target: u16, output: &mut [u8]) {
-	transmute_to_u8_from!(u16, 2, target, output);
+    transmute_to_u8_from!(u16, 2, target, output);
 }
 
 fn u32_to_u8(target: u32, output: &mut [u8]) {
-	transmute_to_u8_from!(u32, 4, target, output);
+    transmute_to_u8_from!(u32, 4, target, output);
 }
 
 fn f32_to_u8(target: f32, output: &mut [u8]) {
-	transmute_to_u8_from!(f32, 4, target, output); 
+    transmute_to_u8_from!(f32, 4, target, output); 
 }
 
 fn append_bytes(bytes: &[u8], buf: &mut[u8], offset: usize) -> usize {
-	if bytes.len() + offset <= buf.len() {
-		for i in 0 .. bytes.len() {
-			buf[i + offset] = bytes[i];
-		}
-	}
-	bytes.len()
+    if bytes.len() + offset <= buf.len() {
+        for i in 0 .. bytes.len() {
+            buf[i + offset] = bytes[i];
+        }
+    }
+    bytes.len()
 }
 
 fn zero_u8_array(targ: &mut [u8]) {
-	for i in 0 .. targ.len() {
-		targ[i] = 0;
-	}
+    for i in 0 .. targ.len() {
+        targ[i] = 0;
+    }
 }
 
 //-------------------------------------------------
 impl WaveHeader {
-	pub fn set_size(&mut self, size: u32){
-		self.file_size = size;
-	}
+    pub fn set_size(&mut self, size: u32){
+        self.file_size = size;
+    }
 }
 
 impl Default for WaveHeader {
-	fn default() -> WaveHeader {
-		WaveHeader { 
-			riff_header: [b'R', b'I', b'F', b'F'],
-			file_size: 0,
-			wave_header: [b'W', b'A', b'V', b'E'],
-			read_cur: 0,
-		}
-	}
+    fn default() -> WaveHeader {
+        WaveHeader { 
+            riff_header: [b'R', b'I', b'F', b'F'],
+            file_size: 0,
+            wave_header: [b'W', b'A', b'V', b'E'],
+            read_cur: 0,
+        }
+    }
 }
 
 impl FormatChunk {
-	pub fn set_number_channels(&mut self, chans: u16) {
-		self.number_channels = chans;
-	}
-	pub fn set_sample_rate(&mut self, rate: u32) {
-		self.samples_second = rate;
-	}
-	pub fn set_byte_rate(&mut self, bytes: u32) {
-		self.bytes_second = bytes;
-	}
-	pub fn set_block_align(&mut self, align: u16 ) {
-		self.block_alignment = align;
-	}
-	pub fn set_bits_sample(&mut self, bits: u16) {
-		self.bits_sample = bits;
-	}
+    pub fn set_number_channels(&mut self, chans: u16) {
+        self.number_channels = chans;
+    }
+    pub fn set_sample_rate(&mut self, rate: u32) {
+        self.samples_second = rate;
+    }
+    pub fn set_byte_rate(&mut self, bytes: u32) {
+        self.bytes_second = bytes;
+    }
+    pub fn set_block_align(&mut self, align: u16 ) {
+        self.block_alignment = align;
+    }
+    pub fn set_bits_sample(&mut self, bits: u16) {
+        self.bits_sample = bits;
+    }
 }
 
 impl Default for FormatChunk {
-	fn default() -> FormatChunk {
-		FormatChunk {
-			fmt_header: [b'f', b'm', b't', b' '],
-			size_wave_chunk: 16,
-			wave_type_format: 1,
-			number_channels: 1,
-			samples_second: 0,
-			bytes_second: 0,
-			block_alignment: 0,
-			bits_sample: 32, //defaulting to F32Sample
-			read_cur: 0,
-		}
-	}
+    fn default() -> FormatChunk {
+        FormatChunk {
+            fmt_header: [b'f', b'm', b't', b' '],
+            size_wave_chunk: 16,
+            wave_type_format: 1,
+            number_channels: 1,
+            samples_second: 0,
+            bytes_second: 0,
+            block_alignment: 0,
+            bits_sample: 32, //defaulting to F32Sample
+            read_cur: 0,
+        }
+    }
 }
 
 impl Read for FormatChunk {
-	fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-		
-		if self.read_cur >= 12 { return Ok(0); }
-		
-		let mut offset: usize = 0;
-		let mut tmb: [u8; 24] = [0; 24];
-		
-		if buf.len() >= 24 {
-			zero_u8_array(buf);
-			for i in 0 .. 4 {
-				tmb[i] = self.fmt_header[i];
-			}
-			offset = 4;
-			
-			do_transmute!(u32_to_u8, self.size_wave_chunk, &mut tmb, &mut offset, 4); 
-			do_transmute!(u16_to_u8, self.wave_type_format, &mut tmb, &mut offset, 2);
-			do_transmute!(u16_to_u8, self.number_channels, &mut tmb, &mut offset, 2);
-			do_transmute!(u32_to_u8, self.samples_second, &mut tmb, &mut offset, 4);
-			do_transmute!(u32_to_u8, self.bytes_second, &mut tmb, &mut offset, 4);
-			do_transmute!(u16_to_u8, self.block_alignment, &mut tmb, &mut offset, 2);
-			do_transmute!(u16_to_u8, self.bits_sample, &mut tmb, &mut offset, 2);
-			append_bytes(&tmb, buf, 0);
-		}
-		self.read_cur = offset;
-		Ok(offset)
-	}
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        
+        if self.read_cur >= 12 { return Ok(0); }
+        
+        let mut offset: usize = 0;
+        let mut tmb: [u8; 24] = [0; 24];
+        
+        if buf.len() >= 24 {
+            zero_u8_array(buf);
+            for i in 0 .. 4 {
+                tmb[i] = self.fmt_header[i];
+            }
+            offset = 4;
+            
+            do_transmute!(u32_to_u8, self.size_wave_chunk, &mut tmb, &mut offset, 4); 
+            do_transmute!(u16_to_u8, self.wave_type_format, &mut tmb, &mut offset, 2);
+            do_transmute!(u16_to_u8, self.number_channels, &mut tmb, &mut offset, 2);
+            do_transmute!(u32_to_u8, self.samples_second, &mut tmb, &mut offset, 4);
+            do_transmute!(u32_to_u8, self.bytes_second, &mut tmb, &mut offset, 4);
+            do_transmute!(u16_to_u8, self.block_alignment, &mut tmb, &mut offset, 2);
+            do_transmute!(u16_to_u8, self.bits_sample, &mut tmb, &mut offset, 2);
+            append_bytes(&tmb, buf, 0);
+        }
+        self.read_cur = offset;
+        Ok(offset)
+    }
 }
 
 impl Read for WaveHeader {
-	fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-		
-		if self.read_cur >= 12 { return Ok(0); }
-		
-		if buf.len() >= 12 {
-			zero_u8_array(buf);
-			let mut tmb: [u8; 13] = [0; 13];
-			let mut off: usize = 4;
-			
-			for i in 0 .. 4 {
-				let x = i + 8;
-				tmb[i] = self.riff_header[i];
-				tmb[x] = self.wave_header[i];
-			}
-			do_transmute!(u32_to_u8, self.file_size, &mut tmb, &mut off, 4); 
-			append_bytes(&tmb, buf, 0);
-			off=12;
-			self.read_cur = off;
-			Ok(off)
-		} else {
-			Err( Error::new(ErrorKind::Other, "Insufficent buffer availible.") )
-		}
-	}
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        
+        if self.read_cur >= 12 { return Ok(0); }
+        
+        if buf.len() >= 12 {
+            zero_u8_array(buf);
+            let mut tmb: [u8; 13] = [0; 13];
+            let mut off: usize = 4;
+            
+            for i in 0 .. 4 {
+                let x = i + 8;
+                tmb[i] = self.riff_header[i];
+                tmb[x] = self.wave_header[i];
+            }
+            do_transmute!(u32_to_u8, self.file_size, &mut tmb, &mut off, 4); 
+            append_bytes(&tmb, buf, 0);
+            off=12;
+            self.read_cur = off;
+            Ok(off)
+        } else {
+            Err( Error::new(ErrorKind::Other, "Insufficent buffer availible.") )
+        }
+    }
 }
 
 impl Read for DataChunk<F32Sample> {
-	fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-		let out_size: usize = self.size_data as usize + 4;
-		let mut tmb: [u8; 4] = [0; 4];
-		let mut off: usize = 0;
-		let mut x: usize = 0;
-		
-		if self.read_cur >= out_size { return Ok(0); }
-		if buf.len() > 8 {
-			if self.read_cur == 0 {
-				zero_u8_array(buf);
-				for i in 0 .. 4 {
-					buf[i] = self.data_header[i];
-				}
-				off = 4;
-				do_transmute!(u32_to_u8, self.size_data, &mut tmb, &mut x, 4);
-				off = off + append_bytes(&tmb, buf, off);
-			}
-			
-			let (_, work_slice) = self.sample_vector.split_at( self.read_cur / 4 );
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let out_size: usize = self.size_data as usize + 4;
+        let mut tmb: [u8; 4] = [0; 4];
+        let mut off: usize = 0;
+        let mut x: usize = 0;
+        
+        if self.read_cur >= out_size { return Ok(0); }
+        if buf.len() > 8 {
+            if self.read_cur == 0 {
+                zero_u8_array(buf);
+                for i in 0 .. 4 {
+                    buf[i] = self.data_header[i];
+                }
+                off = 4;
+                do_transmute!(u32_to_u8, self.size_data, &mut tmb, &mut x, 4);
+                off = off + append_bytes(&tmb, buf, off);
+            }
+            
+            let (_, work_slice) = self.sample_vector.split_at( self.read_cur / 4 );
 
-			for fl in work_slice {
-				println!("fl {}", fl);
-				x = 0;
-				do_transmute!(f32_to_u8, *fl, &mut tmb, &mut x, 4);
-				if (off + 4) < buf.len() {
-					off = off + append_bytes(&tmb, buf, off);
-				} else {
-					self.read_cur = self.read_cur + off;
-					return Ok(off);
-				}
-			}
-			self.read_cur = self.read_cur + off;
-			Ok(off)
-		} else {
-			Err( Error::new(ErrorKind::Other, "Insufficent buffer availible.") )
-		}
-	}
+            for fl in work_slice {
+                println!("fl {}", fl);
+                x = 0;
+                do_transmute!(f32_to_u8, *fl, &mut tmb, &mut x, 4);
+                if (off + 4) < buf.len() {
+                    off = off + append_bytes(&tmb, buf, off);
+                } else {
+                    self.read_cur = self.read_cur + off;
+                    return Ok(off);
+                }
+            }
+            self.read_cur = self.read_cur + off;
+            Ok(off)
+        } else {
+            Err( Error::new(ErrorKind::Other, "Insufficent buffer availible.") )
+        }
+    }
 }
 
 impl DataChunk<F32Sample> {
-	pub fn push_sample(&mut self, sample: F32Sample) {
-		self.sample_vector.push(sample)
-	}
-	pub fn set_size(&mut self, size: u32) {
-		self.size_data = size;
-	}
-	pub fn len(&self) -> usize {
-		self.sample_vector.len()
-	}
+    pub fn push_sample(&mut self, sample: F32Sample) {
+        self.sample_vector.push(sample)
+    }
+    pub fn set_size(&mut self, size: u32) {
+        self.size_data = size;
+    }
+    pub fn len(&self) -> usize {
+        self.sample_vector.len()
+    }
 }
 
 impl Default for DataChunk<F32Sample> {
-	fn default() -> DataChunk<F32Sample> {
-		DataChunk {
-			data_header: [b'd', b'a', b't', b'a'],
-			size_data: 0,
-			sample_vector: Vec::new(),
-			read_cur: 0,
-		}
-	}
+    fn default() -> DataChunk<F32Sample> {
+        DataChunk {
+            data_header: [b'd', b'a', b't', b'a'],
+            size_data: 0,
+            sample_vector: Vec::new(),
+            read_cur: 0,
+        }
+    }
 }
 
 impl WaveFile<F32Sample> {
-	pub fn create_new( hdr: WaveHeader, fmt: FormatChunk, data_in: DataChunk<F32Sample>) -> WaveFile<F32Sample> {
-		WaveFile { header: hdr, format_chunk: fmt, data: data_in, }
-	}
+    pub fn create_new( hdr: WaveHeader, fmt: FormatChunk, data_in: DataChunk<F32Sample>) -> WaveFile<F32Sample> {
+        WaveFile { header: hdr, format_chunk: fmt, data: data_in, }
+    }
 }
