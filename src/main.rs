bb12a35aa0c171022685fdf9aee199b796439b42
Ryan Drew <rsdrew@gmail.com>
2015-05-14 07:58:52 -0400

    Going to make a string sound. Karplus-Strong for fun.

--- src/main.rs
+++ src/main.rs
@@ -97,42 +97,114 @@ impl KarplusStrong {
         KarplusStrong {
             ring: Vec::with_capacity(size),
             ring_first: 0,
-            ring_last: size
-            sample_rate: 44100.0f64
-            frequency: 0,
+            ring_last: size,
+            ring_size: size,
+            sample_rate: 44100.0f64,
+            frequency: 0.0f64,
             ticks: 0,
         }
     }
-    
+
+    fn with_frequency(freq: f64) -> KarplusStrong {
+        let sample_rate = 44100.0f64;
+        let size = (sample_rate / freq).round() as usize;
+        let mut ks = KarplusStrong {
+            ring: Vec::new(),
+            ring_first: 0,
+            ring_last: size,
+            ring_size: size,
+            sample_rate: 44100.0f64,
+            frequency: freq,
+            ticks: 0,
+        };
+
+        let mut i = 0 ;
+        loop {
+            ks.ring.push( 0.0f32 );
+            i = i + 1;
+            if i > size { break; }
+        }
+        ks
+    }
+
     fn set_frequency(&mut self, freq: f64) {
         self.frequency = freq;
         //if freq requires a longer Vec, reallocate
-        //
+        self.ring_size = (self.sample_rate / self.frequency).round() as usize;
+        let cap = self.ring.capacity();
+        if cap < self.ring_size {
+            self.ring.reserve((self.ring_size - cap));
+        }
     }
-    
+
     fn pluck(&mut self) {
-       // Fill self.ring with random noise in range -0.5 to +0.5     
+        use rand::distributions::{IndependentSample, Range};
+        // Fill self.ring with random noise in range -0.5 to +0.5
+        let mut random: rand::OsRng = rand::OsRng::new().unwrap();
+        //let rng: &rand
+        let between = Range::new(-0.50f32, 0.50f32);
+        let mut i: usize = 0;
+
+        loop {
+            let y_val = between.ind_sample(&mut random);
+            let samp = self.ring.get_mut(i).unwrap();
+            *samp = y_val;
+            i = i + 1;
+            if i > self.ring_size { break; }
+        }
+
     }
     
     fn sample(&mut self) -> F32Sample {
-        
+        *self.ring.get(self.ring_first).unwrap()
     }
-    
+
     fn tick_simulation(&mut self) {
-        
+        let next_index = self.ring_first + 1;
+        let mut second: f32 = 0.0f32;
+        // Block to allow borrowing ring twice.
+        {
+            let sr: &f32 = self.ring.get(next_index).unwrap();
+            second = *sr;
+        }
+        let first: &mut f32 = self.ring.get_mut(self.ring_first).unwrap();
+        // Karplus-Strong
+        *first = (*first + second) * 0.50f32 * 0.9940f32;
+        self.ring_last = self.ring_first;
+        if next_index >= self.ring_size { self.ring_first = 0; }
+        else { self.ring_first = next_index; }
+        self.ticks = self.ticks + 1;
     }
-    
-    fn get_tick(&mut self) {
-        
+
+    fn get_ticks(&mut self) -> u64 {
+        self.ticks
+    }
+
+    fn get_ring_size(&self) -> usize {
+    	self.ring_size
     }
 }
+
 
 
 fn main() {
     use std::io::copy;
 
-    let y = generate_tone(2.0f64, 440.0f64, 44100, 32);
-    let mut x = create_mono_wave_file(y, 44100, 32);
+    let mut ks: KarplusStrong = KarplusStrong::with_frequency(440.0f64);
+    
+    ks.pluck();
+    
+    let mut i = 0;
+    loop {
+        ks.tick_simulation();
+        let t = ks.get_ticks();
+        let samp: F32Sample = ks.sample();
+        println!("t={} s={}", t, samp);
+        if i > 1000 { break; }
+        i = i + 1;
+    }
+    //let y = generate_tone(2.0f64, 440.0f64, 44100, 32);
+    //let mut x = create_mono_wave_file(y, 44100, 32);
     //let mut z: Vec<u8> = Vec::new();
 
     /*let _ = x.header.read_to_end(&mut z);
@@ -141,7 +213,7 @@ fn main() {
     let _ = x.format_chunk.read_to_end(&mut z);
     println!("fmt - {:?} ", z);*/
 
-    let mut f = File::create("test_440.wav").unwrap();
+    /*let mut f = File::create("test_440.wav").unwrap();
 
     //try!( f.write_all( x.header.read_to_end()));
     let _ = copy( &mut x.header, &mut f);
@@ -149,5 +221,7 @@ fn main() {
     let _ = copy( &mut x.format_chunk, &mut f);
     //try!( f.write_all( x.data.read_to_end()));
     let _ = copy( &mut x.data, &mut f);
-    f.sync_all();
+    f.sync_all();*/
+
+    
 }
