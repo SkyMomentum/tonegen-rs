53629654eb509d0be5b9ffdb2de4c5573496625a
Ryan Drew <rsdrew@gmail.com>
2015-05-15 07:15:10 -0400

    Disentangle synth mod functions from wav file library.

--- src/synth/tone.rs
+++ src/synth/tone.rs
@@ -1,15 +1,14 @@
-use wavout::{F32Sample, DataChunk};
 use std::f64::consts;
 
 /// Fill a Vec<> with a single cycle at frequency and sample rate.
-pub fn create_sine_sample(frequency: f64, sample_rate: u32) -> Vec<F32Sample> {
+pub fn create_sine_sample(frequency: f64, sample_rate: u32) -> Vec<f32> {
 
     let sr: f64 = sample_rate as f64;
     let samples_sine: f64 = sr / frequency;
     let samples_num: u32 = samples_sine.floor() as u32;
     let half_wave: f64 = samples_sine / 2.0f64;
     
-    let mut tone_cycle: Vec<F32Sample> = Vec::with_capacity(samples_num as usize);
+    let mut tone_cycle: Vec<f32> = Vec::with_capacity(samples_num as usize);
     
     for i in 0..samples_num {
         let x: f64 = i as f64;
@@ -25,11 +24,11 @@ pub fn create_sine_sample(frequency: f64, sample_rate: u32) -> Vec<F32Sample> {
 /// Create a data chunk with specified run length, frequency, and sample details.
 ///
 /// Current support functions only provide 32bit sample size.
-fn generate_tone(run_length: f64, frequency: f64, sample_rate: u32, sample_bits: u32) -> DataChunk<F32Sample> {
+fn generate_tone_f32(run_length: f64, frequency: f64, sample_rate: u32) -> Vec<f32> {
 
-    let mut tone_out: DataChunk<F32Sample> = Default::default();
+    let mut tone_out: Vec<f32> = Vec::new();
 
-    let tone_cycle: Vec<F32Sample> = create_sine_sample(frequency, sample_rate);
+    let tone_cycle: Vec<f32> = create_sine_sample(frequency, sample_rate);
     let cycle_len: usize = tone_cycle.len();
 
     let sr: f64 = sample_rate as f64;
@@ -42,11 +41,10 @@ fn generate_tone(run_length: f64, frequency: f64, sample_rate: u32, sample_bits:
     loop {
         if out_counter > total_samples { break; }
         if cycle_index >= cycle_len { cycle_index = 0; }
-        tone_out.push_sample( *(tone_cycle.get(cycle_index).unwrap()) );
+        tone_out.push( *(tone_cycle.get(cycle_index).unwrap()) );
         out_counter = out_counter + 1;
         cycle_index = cycle_index + 1;
     }
 
-    tone_out.set_size( out_counter * (sample_bits / 8) );
     tone_out
 }
