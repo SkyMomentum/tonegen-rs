de2d7b954bb39eceac5cfe77141ffe565cdc6645
Ryan Drew <rsdrew@gmail.com>
2017-08-01 20:01:09 -0400

    Tiny fix so this compiles again. Updated Cargo.toml for bin path. Added a .gitignore.

--- src/synth/tone.rs
+++ src/synth/tone.rs
@@ -6,12 +6,12 @@ pub fn create_sine_sample(frequency: f64, sample_rate: f64) -> Vec<f32> {
     let samples_sine: f64 = sample_rate / frequency;
     let samples_num: u32 = samples_sine.floor() as u32;
     let half_wave: f64 = samples_sine / 2.0f64;
-    
+
     let mut tone_cycle: Vec<f32> = Vec::with_capacity(samples_num as usize);
-    
+
     for i in 0..samples_num {
         let x: f64 = i as f64;
-        let val_calc: f64 = ((x * consts::PI)/half_wave).sin();
+        let val_calc: f64 = ((x * consts::PI) / half_wave).sin();
         let val_out: f32 = val_calc as f32;
         tone_cycle.push(val_out);
     }
@@ -35,9 +35,13 @@ pub fn generate_tone_f32(run_length: f64, frequency: f64, sample_rate: f64) -> V
     let mut cycle_index: usize = 0;
 
     loop {
-        if out_counter > total_samples { break; }
-        if cycle_index >= cycle_len { cycle_index = 0; }
-        tone_out.push( *(tone_cycle.get(cycle_index).unwrap()) );
+        if out_counter > total_samples {
+            break;
+        }
+        if cycle_index >= cycle_len {
+            cycle_index = 0;
+        }
+        tone_out.push(*(tone_cycle.get(cycle_index).unwrap()));
         out_counter = out_counter + 1;
         cycle_index = cycle_index + 1;
     }
@@ -51,22 +55,26 @@ pub fn generate_tone_u8(run_length: f64, frequency: f64, sample_rate: f64) -> Ve
 
     let tone_cycle: Vec<f32> = create_sine_sample(frequency, sample_rate);
     let cycle_len: usize = tone_cycle.len();
-    
-    let mut scaled_tone_cycle: Vec<u8> =  Vec::new();
+
+    let mut scaled_tone_cycle: Vec<u8> = Vec::new();
     for s in tone_cycle {
-        let scaled_sample: u8 = ((s + 0.5f64) * 256.0).round() as u8;
-        scaled_tone_cycle.push( scaled_sample );
+        let scaled_sample: u8 = ((s + 0.5f32) * 256.0).round() as u8;
+        scaled_tone_cycle.push(scaled_sample);
     }
-    
+
     let total_samples: u32 = ((run_length * sample_rate).floor()) as u32;
 
     let mut out_counter: u32 = 0;
     let mut cycle_index: usize = 0;
 
     loop {
-        if out_counter > total_samples { break; }
-        if cycle_index >= cycle_len { cycle_index = 0; }
-        tone_out.push( *(scaled_tone_cycle.get(cycle_index).unwrap()) );
+        if out_counter > total_samples {
+            break;
+        }
+        if cycle_index >= cycle_len {
+            cycle_index = 0;
+        }
+        tone_out.push(*(scaled_tone_cycle.get(cycle_index).unwrap()));
         out_counter = out_counter + 1;
         cycle_index = cycle_index + 1;
     }
